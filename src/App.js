bd2b1d40ea86d9bac34bd9a8c3e6a5b980e4b863
Izaak Whetsell <izaakwhetsell@gmail.com>
2023-11-09 21:06:36 -0500

    Lab 8 Submission

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import './App.css';
-
+import { useState } from 'react';
+import './Tasks.css';
 
 function App() {
-  return (
-    <div>
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+  const addItem = () =>{
+    if(value!==""){
+      setList([...list, value])
+      setValue("")
+    }
+  }
+  const removeItem= index =>{
+    setList(oldList=>{
+      return oldList.filter((_, i) => i !== index)
+    })
+   
+  }
 
+//call color as color{}
+  return (
+    <div className='containerr'>
+      <div className="body">
+      <h1>ToDo List</h1> 
+        <div>
+          <input value={value} onChange={(e)=>{setValue(e.target.value)}} class="form-control" placeholder="Enter Task"/>
+          <button onClick={addItem} className="btn btn-primary my-3">Add Task</button>
+        </div>
+     
+      <ul class="list-group">
+        {list.map((item,index)=>{
+          return (<li key={index}>{item} 
+          <span onClick={() => removeItem(index)}>X</span></li>);
+        })}
+      </ul>
     </div>
-  );
+    </div>
+  );//even numbers different from odd numbers, h<over has diverent cover. Use color or picture as background.
 }
 
 export default App;
